8955c4e833379032d0445d412518c140b73a042f
qwe1357 <76419984+qwe1357@users.noreply.github.com>
2022-01-21 21:49:08 +0900

    f

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -15,12 +17,13 @@ mongoose
     .then(() => console.log("DB Connection Successfull!"))
     .catch((err) => console.log(err));
 
-
 app.use(express.json());
 //요청의 본문에 있는 데이터를 해석해서 req.body객체를 만들어주는 미들웨어
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running");
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-const verify = require('../verifyToken');
+const verify = require("../verifyToken");
 //UPDATE
 
-
-router.put("/:id", verify,async (req, res) => {
+router.put("/:id", verify, async (req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         if (req.body.password) {
             req.body.password = CryptoJS.AES.encrypt(
@@ -15,7 +14,13 @@ router.put("/:id", verify,async (req, res) => {
         }
 
         try {
-            const updatedUser=await User.findByIdAndUpdate(req.params.id,{$set:req.body});
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
             res.status(200).json(updatedUser);
         } catch (err) {
             res.status(500).json(err);
@@ -25,8 +30,66 @@ router.put("/:id", verify,async (req, res) => {
     }
 });
 //DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            await User.findOneAndDelete(req.params.id);
+            res.status(200).json("User has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You  can delete only your account!");
+    }
+});
 //GET
-//GET ALL
-//GET USER STATS
+router.get("/find/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...info } = user._doc;
 
-module.exports=router;
+        res.status(200).json(info);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//GET ALL
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query
+                ? await User.find().limit(10)
+                : await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all users!");
+    }
+});
+//GET USER STATS
+router.get("/stats", async (req, res) => {
+    
+    try {
+        const data = await User.aggregate([
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 },
+                },
+            },
+        ]);
+        res.status(200).json(data);
+    } catch {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
