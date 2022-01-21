a90d1d28c02c9fc6803a5101d107bd8d099781c8
qwe1357 <76419984+qwe1357@users.noreply.github.com>
2022-01-22 00:48:05 +0900

    redirect..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import "./app.scss"
-//import Home from './pages/home/Home';
-//import Watch from './pages/watch/Watch';
-import Register from './pages/register/Register';
-//import Login from './pages/login/Login';
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+// import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-const App=()=>{
-  return <Register/>
+const App = () => {
+    const user = false;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/">
+                    {user ? <Home /> : <Navigate to="/register" replace={true}/>}
+                </Route>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/movies" element={<Home type="movies" />} />
+                <Route path="/series" element={<Home type="series" />} />
+                <Route path="/watch" element={<Watch />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 export default App;
