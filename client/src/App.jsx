c6afaaeb62b2048b620c4d33619f4d8ccf1c4187
qwe1357 <76419984+qwe1357@users.noreply.github.com>
2022-01-22 01:36:49 +0900

    er list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,31 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route  path="/" element={user? <Home/>:<Navigate to="/register"/>}/>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/movies" element={<Home type="movies" />} />
-                <Route path="/series" element={<Home type="series" />} />
-                <Route path="/watch" element={<Watch />} />
+                <Route
+                    path="/"
+                    element={user ? <Home /> : <Navigate to="/register" />}
+                />
+                <Route
+                    path="/register"
+                    element={user ? <Register /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/login"
+                    element={!user ? <Login /> : <Navigate to="/" />}
+                />
+                {user && (
+                    <>
+                        <Route
+                            path="/movies"
+                            element={<Home type="movies" />}
+                        />
+                        <Route
+                            path="/series"
+                            element={<Home type="series" />}
+                        />
+                        <Route path="/watch" element={<Watch />} />
+                    </>
+                )}
             </Routes>
         </BrowserRouter>
     );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,15 +2,38 @@ import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
-const Home = ({type}) => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+const Home = ({ type }) => {
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+    useEffect(() => {
+        const getRandomLists = async () => {
+            try {
+                const res = await axios.get(
+                    `lists${type ? "?type=" + type : ""}&${
+                        genre ? "&genre=" + genre : ""
+                    }`,
+                    {
+                        headers: {
+                            token: "Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWE4MTM0MzFhYWNjYjYyOTM3YTc0MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0Mjc4MjMxOSwiZXhwIjoxNjQzMjE0MzE5fQ.tMXnWvlmBD1coqdaOFHciibMIcqKkL70bXCSAIFAZtk",
+                        },
+                    }
+                );
+                setLists(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getRandomLists();
+    }, [type, genre]); //배열 안 변수의 값이 변할 때마다 실행하는 코드
     return (
         <div className="home">
             <Navbar />
-            <Featured type={type} />
-            <List />
-            <List />
-            <List />
-            <List />
+            <Featured type={type} setGenre={setGenre} />
+            {lists.map((list) => (
+                <List list={list} />
+            ))}
         </div>
     );
 };
